11918afd7f6e0696abef7ad6d63c2b0997d3a68a
Niklas Wilming <niklaswilming@fastmail.fm>
2016-09-27 10:24:52 +0200

    Add setup to make installable

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pyMEG',
+      version='0.0.1',
+      description='Python MEG tools for preprocessing etc.',
+      author='Niklas Wilming',
+      author_email='nwilming@uke.de',
+      url='https://github.com/DonnerLab/pymeg',
+      packages=['meg'])
