2bbc3e2aa33a577a02e4ec4000af5d634febf831
Niklas Wilming <niklaswilming@fastmail.fm>
2018-07-17 10:51:06 +0200

    create reconall script that submits to cluster

--- scripts/reconall.sh
+++ scripts/reconall.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo '#!/bin/sh
+
 #PBS -q batch
 #PBS -l walltime=500:00:00
 #PBS -l nodes=1:ppn=1
@@ -12,5 +14,7 @@ chmod g=wx $PBS_JOBNAME
 
 export SUBJECTS_DIR=${subjectdir}
 
-recon-all -subject ${subid} -i ${nifti} -all 1> "$PBS_JOBID"1.out 2> "$PBS_JOBID"1.err
+recon-all -subject ${subid} -i ${nifti} -all 1> "$PBS_JOBID"1.out 2> "$PBS_JOBID"1.err' >> _reconall.sh
+
+qsub -v subid=$1 -v nifti=$2 -v subjectdir=$3 _reconall.sh
 
--- scripts/submit_subject.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-qsub -v subid=$1 -v nifti=$2 -v subjectdir=$3 reconall.sh
-
