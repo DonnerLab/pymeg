89487f094e7952d87c7854890f0452d7d1de69b0
Niklas Wilming <nwilming@uke.de>
2018-07-31 18:35:13 +0200

    Add script to submit a recon_all to PBS cluster

--- scripts/reconall.sh
+++ scripts/reconall.sh
@@ -1,20 +1,18 @@
 #!/bin/sh
 
-echo '#!/bin/sh
+echo "#!/bin/sh
 
 #PBS -q batch
-#PBS -l walltime=500:00:00
-#PBS -l nodes=1:ppn=1
-#PBS -l pmem=14gbs
+#PBS -l walltime=10:00:00
 
 # -- run in the current working (submission) directory --
-cd $PBS_O_WORKDIR
+cd \$PBS_O_WORKDIR
 
-chmod g=wx $PBS_JOBNAME
+chmod g=wx \$PBS_JOBNAME
 
-export SUBJECTS_DIR=${subjectdir}
+export SUBJECTS_DIR=${3}
 
-recon-all -subject ${subid} -i ${nifti} -all 1> "$PBS_JOBID"1.out 2> "$PBS_JOBID"1.err' >> _reconall.sh
+recon-all -subjid $1 -i $2 -all 1> \$PBS_JOBID.out 2> \$PBS_JOBID.err" > _reconall.sh
 
-qsub -v subid=$1 -v nifti=$2 -v subjectdir=$3 _reconall.sh
+qsub _reconall.sh
 
