a7908ea775886f1a2090a891dae5f85b770c46a0
Niklas Wilming <niklaswilming@fastmail.fm>
2019-05-02 14:42:55 +0200

    Reformat with sublack

--- pymeg/aggregate_sr.py
+++ pymeg/aggregate_sr.py
@@ -51,10 +51,15 @@ import pandas as pd
 import numpy as np
 
 
-def aggregate_files(data_globstring, base_globstring, baseline_time,
-                    hemis=['Averaged', 'Pair', 'Lateralized'],
-                    cache=None, to_decibels=False,
-                    all_clusters=None):
+def aggregate_files(
+    data_globstring,
+    base_globstring,
+    baseline_time,
+    hemis=["Averaged", "Pair", "Lateralized"],
+    cache=None,
+    to_decibels=False,
+    all_clusters=None,
+):
     """Read source reconstructed files, baseline correct and aggregate into
     area clusters.
 
@@ -81,13 +86,14 @@ def aggregate_files(data_globstring, base_globstring, baseline_time,
         by time, frequency and cluster in the row index.
     """
     from pymeg.contrast_tfr import Cache
+
     tfr_baseline = None
     if not (data_globstring == base_globstring):
         with Cache() as base_cache:
             tfr_baseline = base_cache.get(base_globstring)
             if to_decibels:
                 tfr_baseline = 10 * np.log10(tfr_baseline)
-            tfr_baseline = tfr_baseline.groupby(['freq', 'area']).mean()
+            tfr_baseline = tfr_baseline.groupby(["freq", "area"]).mean()
 
     if cache is None:
         cache = Cache()
@@ -95,18 +101,19 @@ def aggregate_files(data_globstring, base_globstring, baseline_time,
     if to_decibels:
         tfr_data = 10 * np.log10(tfr_data)
     if tfr_baseline is None:
-        tfr_baseline = tfr_data.groupby(['freq', 'area']).mean()
+        tfr_baseline = tfr_data.groupby(["freq", "area"]).mean()
 
     baseline = tfr_baseline.loc[:, slice(*baseline_time)].mean(1)
-    baseline.name = 'baseline'
+    baseline.name = "baseline"
     cols = tfr_data.columns
-    tfr_data = tfr_data.join(baseline, on=['freq', 'area'])
-    baseline = tfr_data.loc[:, 'baseline']
+    tfr_data = tfr_data.join(baseline, on=["freq", "area"])
+    baseline = tfr_data.loc[:, "baseline"]
     if to_decibels:
         tfr_data = tfr_data.loc[:, cols].sub(baseline, axis=0)
     else:
-        tfr_data = ((tfr_data.loc[:, cols].sub(
-            baseline, axis=0)).div(baseline, axis=0)) * 100
+        tfr_data = (
+            (tfr_data.loc[:, cols].sub(baseline, axis=0)).div(baseline, axis=0)
+        ) * 100
     aggs = aggregate(tfr_data, hemis, all_clusters=all_clusters)
     return aggs
 
@@ -128,31 +135,32 @@ def agg2hdf(agg, filename):
             Path to file.
     """
     import h5py
-    with h5py.File(filename, mode='w') as store:
-        for (hemi, cluster, freq), data in agg.groupby(['hemi', 'cluster', 'freq']):
+
+    with h5py.File(filename, mode="w") as store:
+        for (hemi, cluster, freq), data in agg.groupby(["hemi", "cluster", "freq"]):
             try:
-                grp = store.create_group(hemi + '/' + cluster)
+                grp = store.create_group(hemi + "/" + cluster)
             except ValueError:
-                grp = store[hemi + '/' + cluster]
+                grp = store[hemi + "/" + cluster]
             dset = grp.create_dataset(
-                str(freq), data=data.values, compression="gzip", compression_opts=7)
-            dset.attrs['cols_time'] = data.columns.values.astype(float)
+                str(freq), data=data.values, compression="gzip", compression_opts=7
+            )
+            dset.attrs["cols_time"] = data.columns.values.astype(float)
             for index in data.index.names:
                 index_vals = data.index.get_level_values(index).values
                 if index_vals.dtype == object:
-                    index_vals = [str(i).encode('utf-8') for i in index_vals]
-                dset.attrs[
-                    'rows_' + index] = index_vals
+                    index_vals = [str(i).encode("utf-8") for i in index_vals]
+                dset.attrs["rows_" + index] = index_vals
 
 
 def delayed_agg(filename, hemi=None, cluster=None, freq=None):
     from functools import partial
+
     return partial(hdf2agg, filename, hemi=hemi, cluster=cluster, freq=freq)
 
 
 def hdf2agg(filenames, hemi=None, cluster=None, freq=None):
-    return pd.concat([_hdf2agg(f, hemi, cluster, freq)
-                      for f in ensure_iter(filenames)])
+    return pd.concat([_hdf2agg(f, hemi, cluster, freq) for f in ensure_iter(filenames)])
 
 
 def _hdf2agg(filename, hemi=None, cluster=None, freq=None):
@@ -171,15 +179,16 @@ def _hdf2agg(filename, hemi=None, cluster=None, freq=None):
         and time in the columns.
     """
     import h5py
+
     dfs = []
-    with h5py.File(filename, mode='r') as store:
+    with h5py.File(filename, mode="r") as store:
         for fhemi, hd in store.items():
             if hemi is not None and not (str(hemi) == fhemi):
                 continue
             for fcluster, cd in hd.items():
                 if (cluster is not None) and (
-                        not any([str(c) == fcluster for c in
-                                 ensure_iter(cluster)])):
+                    not any([str(c) == fcluster for c in ensure_iter(cluster)])
+                ):
                     continue
                 for fF, Fd in cd.items():
                     if freq is not None and not (str(freq) == fF):
@@ -199,18 +208,18 @@ def get_df_from_hdf(dataset):
     for key, values in dataset.attrs.items():
         dname = values.dtype.name
 
-        if 'bytes' in dname:
-            values = values.astype('U')
+        if "bytes" in dname:
+            values = values.astype("U")
             try:
                 values = [float(i) for i in values]
             except ValueError:
                 pass
-        if key.startswith('cols_'):
+        if key.startswith("cols_"):
             cols.append(values)
-            col_names.append(key.replace('cols_', ''))
-        if key.startswith('rows_'):
+            col_names.append(key.replace("cols_", ""))
+        if key.startswith("rows_"):
             rows.append(values)
-            row_names.append(key.replace('rows_', ''))
+            row_names.append(key.replace("rows_", ""))
     index = pd.MultiIndex.from_arrays(rows)
     index.names = row_names
 
@@ -224,44 +233,50 @@ def aggregate(tfr_data, hemis, all_clusters=None):
     """
     from itertools import product
     from pymeg import atlas_glasser
+
     if all_clusters is None:
         all_clusters, _, _, _ = atlas_glasser.get_clusters()
     clusters = []
-    tfr_areas = np.unique(tfr_data.index.get_level_values('area'))
+    tfr_areas = np.unique(tfr_data.index.get_level_values("area"))
     for hemi, cluster in product(hemis, all_clusters.keys()):
-        print('Working on %s, %s' % (hemi, cluster))
-        tfrs_rh = [area for area in all_clusters[cluster] if 'rh' in area]
-        tfrs_lh = [area for area in all_clusters[cluster] if 'lh' in area]
-        tfrs_rh = [t for t in tfr_areas if any(
-            [a.lower() in t.lower() for a in tfrs_rh])]
-        tfrs_lh = [t for t in tfr_areas if any(
-            [a.lower() in t.lower() for a in tfrs_lh])]
-        lh_idx = tfr_data.index.isin(tfrs_lh, level='area')
-        rh_idx = tfr_data.index.isin(tfrs_rh, level='area')
-        left = tfr_data.loc[lh_idx, :].groupby(
-            ['freq', 'trial']).mean()
-        right = tfr_data.loc[rh_idx, :].groupby(
-            ['freq', 'trial']).mean()
+        print("Working on %s, %s" % (hemi, cluster))
+        tfrs_rh = [area for area in all_clusters[cluster] if "rh" in area]
+        tfrs_lh = [area for area in all_clusters[cluster] if "lh" in area]
+        tfrs_rh = [
+            t for t in tfr_areas if any([a.lower() in t.lower() for a in tfrs_rh])
+        ]
+        tfrs_lh = [
+            t for t in tfr_areas if any([a.lower() in t.lower() for a in tfrs_lh])
+        ]
+        lh_idx = tfr_data.index.isin(tfrs_lh, level="area")
+        rh_idx = tfr_data.index.isin(tfrs_rh, level="area")
+        left = tfr_data.loc[lh_idx, :].groupby(["freq", "trial"]).mean()
+        right = tfr_data.loc[rh_idx, :].groupby(["freq", "trial"]).mean()
+        if (len(left) == 0) or (len(right) == 0):
+            print(
+                "%s: Left or right Hemi is empty, skipping for aggregation"
+                % (cluster)
+            )
+            continue
 
-        if hemi == 'Pair':
-            left.loc[:, 'cluster'] = cluster + '_LH'
-            left.loc[:, 'hemi'] = 'Pair'
-            right.loc[:, 'cluster'] = cluster + '_RH'
-            right.loc[:, 'hemi'] = 'Pair'
+        if hemi == "Pair":
+            left.loc[:, "cluster"] = cluster + "_LH"
+            left.loc[:, "hemi"] = "Pair"
+            right.loc[:, "cluster"] = cluster + "_RH"
+            right.loc[:, "hemi"] = "Pair"
             clusters.append(left)
             clusters.append(right)
         else:
-            if hemi == 'Lateralized':
+            if hemi == "Lateralized":
                 tfrs = left - right
-            elif hemi == 'Averaged':
+            elif hemi == "Averaged":
                 tfrs = (right + left) / 2
-            tfrs.loc[:, 'cluster'] = cluster
-            tfrs.loc[:, 'hemi'] = hemi
+            tfrs.loc[:, "cluster"] = cluster
+            tfrs.loc[:, "hemi"] = hemi
             clusters.append(tfrs)
     df = pd.concat(clusters)
-    df.set_index(['cluster', 'hemi'], append=True, inplace=True)
-    return df.reorder_levels(
-        ['hemi', 'cluster', 'trial', 'freq'])
+    df.set_index(["cluster", "hemi"], append=True, inplace=True)
+    return df.reorder_levels(["hemi", "cluster", "trial", "freq"])
 
 
 def ensure_iter(input):
